569c7835bc955d7283a1b3dd5362cb670ae7d46b
Beau Simensen <beau@dflydev.com>
2015-06-02 13:14:12 -0500

    In fact, testing does not need composer. Or travis.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../autoload.php';
 require_once __DIR__.'/util.php';
 require_once __DIR__.'/fixtures.php';
 require_once __DIR__.'/DummyAsendiaWsdlClient.php';
